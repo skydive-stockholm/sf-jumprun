5223f60ba9e9cd9c0fcf7827d34352dbfc79509e
robin <hej@robin.se>
2023-07-09 19:46:33 +0200

    Added last gust from Insidan API

--- src/components/JumpRunInfoBox.vue
+++ src/components/JumpRunInfoBox.vue
@@ -1,5 +1,9 @@
 <template>
     <section :class="$style.infoBox">
+        <div :class="$style.lastGust">
+            <strong>Last gust (30 min):</strong> {{ lastGust }}
+        </div>
+
         <div :class="$style.phoneNumber">
             <strong>Tel manifest:</strong> 076 135 43 85
         </div>
@@ -22,11 +26,18 @@
 
 <script setup>
 import { ref } from 'vue'
+import axios from 'axios'
 
 const staff = ref({
     jumpLeader: 'Stefan Burström',
     manifestor: 'Felix Holmertz',
     pilot: 'Andreas Henriksson',
+})
+
+const lastGust = ref(null)
+
+axios.get('https://insidan.skydive.se/api/weather').then(res => {
+    lastGust.value = res.data.lastGust30Min
 })
 </script>
 
@@ -43,5 +54,8 @@ const staff = ref({
     display: flex;
     gap: 20px;
     flex-direction: column;
+}
+
+.lastGust {
 }
 </style>
